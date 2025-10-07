9003f251d62a3b90228bd5875c0dd0ee96fdee6d
Patlix Studio <patlixstudio@gmail.com>
2025-10-07 07:00:27 +0300

    marine-services mock data

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, ViewEncapsulation } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject,  } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
--- src/app/pages/marine-services/marine-services.ts
+++ src/app/pages/marine-services/marine-services.ts
@@ -1,40 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MarineServicesService } from '../../services/marine-services.service';
+import { MARINE_SERVICE_CATEGORIES } from '../../core/constants/marine-service.categories';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-marine-services',
-  imports: [CommonModule, MatInputModule, FormsModule, MatCardModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, MatInputModule, FormsModule, MatCardModule, MatIconModule, MatButtonModule,MatSelectModule, MatOptionModule],
   templateUrl: './marine-services.html',
   styleUrl: './marine-services.scss'
 })
 export class MarineServices {
+  marineService = inject(MarineServicesService);
+
   filters = { type: '', location: '' };
 
-  services = [
-    {
-      name: 'Atlantic Ship Maintenance',
-      type: 'Maintenance',
-      location: 'Miami, USA',
-      description: 'Full-service vessel maintenance including engine, hull, and electronics.'
-    },
-    {
-      name: 'Ocean Training Academy',
-      type: 'Training',
-      location: 'Rotterdam, Netherlands',
-      description: 'Professional maritime training for captains, engineers, and crew.'
-    },
-    {
-      name: 'Global Charter Support',
-      type: 'Logistics',
-      location: 'Singapore',
-      description: 'End-to-end support for yacht and cargo charter operations worldwide.'
-    }
-  ];
+  services = this.marineService.services();
+  serviceCategories = MARINE_SERVICE_CATEGORIES;
+
+  ngOnInit() {
+    
+    console.log(this.marineService.fetchServices()); // always fetch fresh listings
+  }
 
   searchServices() {
     console.log('Searching services with filters:', this.filters);
