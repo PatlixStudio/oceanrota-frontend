2ca53b1cde7781688c1d5715cf8d9eca4593e775
Patlix Studio <patlixstudio@gmail.com>
2026-03-08 13:59:55 +0800

    env files

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: false,
+  name: 'local',
+
+  apiUrl: 'http://localhost:3000',
+
+  stripe: {
+    publishableKey: 'pk_test_xxxxxxxxx'
+  },
+
+  marketplace: {
+    name: 'OceanRota',
+    featuredEnabled: true,
+    sponsorsEnabled: true
+  }
+};
